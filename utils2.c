732a501c52d775df899bea12e3afcd5946a056ad
ubuntu <togokoegele@gmail.com>
2025-01-16 16:59:49 +0000

    norminette

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:07:36 by logkoege          #+#    #+#             */
-/*   Updated: 2025/01/16 16:56:22 by logkoege         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:59:36 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int	token_is_valid(char *input)
 	}
 	return (1);
 }
-int quote_not_closed(t_data *data)
+
+int	quote_not_closed(t_data *data)
 {
 	if (data->single_quote)
 	{
