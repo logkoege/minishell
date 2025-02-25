981c185765bf12f0c877e96f00892ccd5a11ff47
ubuntu <togokoegele@gmail.com>
2025-02-25 13:41:28 +0100

    dernier arg = NULL + fix des autres bugs

--- last_chainlist.c
+++ last_chainlist.c
@@ -6,7 +6,7 @@
 /*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:20:11 by logkoege          #+#    #+#             */
-/*   Updated: 2025/02/25 13:26:07 by logkoege         ###   ########.fr       */
+/*   Updated: 2025/02/25 13:40:55 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,35 +73,40 @@ void	first_to_cmd(t_data *data)
 	{
 		if (tmp->token == PIPE)
 		{
+			cmd->arg[i] = NULL;
+			cmd->file[j] = NULL;
 			print_lst_cmd(cmd);
 			lstadd_back_cmd(&cmd, lst_new_cmd(data));
 			k = 0;
 			i = 0;
 			j = 0;
+			tmp = tmp->next;
 		}
-		else if (tmp->token == HEREDOC || tmp->token == TRUNC
+		if (tmp->token == HEREDOC || tmp->token == TRUNC
 			|| tmp->token == INPUT || tmp->token == APPEND)
 		{
 			cmd->tkn[k] = tmp->token;
-			cmd->tkn[k + 1] = 0;
 			tmp = tmp->next;
 			cmd->file[j] = tmp->str;
+			cmd->file[j + 1] = NULL;
 			j++;
 			k++;
 		}
 		else if (tmp->token == WORD)
 		{
 			cmd->arg[i] = delete_quote(tmp->str);
+			cmd->arg[i + 1] = NULL;
 			cmd->tkn[k] = tmp->token;
 			i++;
 			k++;
 		}
-		tmp = tmp->next;
 		if (tmp == NULL)
 		{
 			cmd->arg[i] = NULL;
 			cmd->file[j] = NULL;
 		}
+		else
+			tmp = tmp->next;
 	}
 	if (tmp == NULL)
 	{
