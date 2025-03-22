4845330d33ad60304ffc5f07f4ccd57175750bc9
ubuntu <togokoegele@gmail.com>
2025-03-22 22:42:17 +0100

    fix

--- minishell2/parsing/dollar.c
+++ minishell2/parsing/dollar.c
@@ -6,7 +6,7 @@
 /*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 17:10:00 by logkoege          #+#    #+#             */
-/*   Updated: 2025/03/20 17:42:45 by logkoege         ###   ########.fr       */
+/*   Updated: 2025/03/21 15:34:15 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ int	dollar_changer(t_first *tmp, int i, t_env *env, bool quote, t_data *data)
 	char	*str;
 
 	i++;
-	tenv2 = dollar_cmp(tmp, env, i, quote);
+	tenv2 = dollar_cmp(tmp, env, i);
 	if (tenv2 != NULL)
 	{
 		str = malloc(sizeof(char) * (ft_strlen(tmp->str)
@@ -92,7 +92,7 @@ int	dollar_changer(t_first *tmp, int i, t_env *env, bool quote, t_data *data)
 	return (0);
 }
 
-t_env	*dollar_cmp(t_first *tmp, t_env *env, int i, bool quote)
+t_env	*dollar_cmp(t_first *tmp, t_env *env, int i)
 {
 	t_env	*tenv;
 	int		j;
@@ -109,9 +109,11 @@ t_env	*dollar_cmp(t_first *tmp, t_env *env, int i, bool quote)
 		{
 			if (tenv->before_eq[j] == '\0')
 			{
-				if (quote == true && tmp->str[i] == '\"')
-					return (tenv);
-				else if ((tmp->str[i] == '\0' || tmp->str[i] == ' '))
+				// if (quote == false && tmp->str[i] == '\"')
+				// {
+				// 	return (tenv);
+				// }
+				if ((tmp->str[i] == '\0' || tmp->str[i] == ' ') || tmp->str[i] == '\"')
 					return (tenv);
 				else
 					break ;
--- minishell2/parsing/utils.c
+++ minishell2/parsing/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: levaipro <levaipro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 15:10:01 by logkoege          #+#    #+#             */
-/*   Updated: 2025/03/21 16:47:27 by levaipro         ###   ########.fr       */
+/*   Updated: 2025/03/18 16:14:15 by logkoege         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	if(!str)
-		return(0);
 	while (str[i])
 		i++;
 	return (i);
@@ -55,7 +53,6 @@ int	ft_strlen(char *str)
 
 void	init_var(t_data *data, int argc, char **argv)
 {
-	// exit_code = 0;
 	(void)argc;
 	(void)argv;
 	data->exit_code = 0;
